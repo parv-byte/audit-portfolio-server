61990ea06bbfab5ce0077129f1e8ec8e31a10e05
parv-byte <rastogiparv480@gmail.com>
2026-03-31 17:32:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: true,
   credentials: true,
 }));
 app.use(express.json({ limit: '50mb' }));
